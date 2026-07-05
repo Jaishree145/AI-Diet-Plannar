28354a150724c852de693f64080f0c531ba82687
Jaishree145 <jaishreekandasamy145@gmail.com>
2026-07-05 09:19:23 +0530

    Add files via upload

---
+++ src/main/java/ai_diet_planner/AiDietPlannerApplication.java
@@ -0,0 +1,13 @@
+package ai_diet_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDietPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDietPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai_diet_planner/AiDietPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_diet_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDietPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
